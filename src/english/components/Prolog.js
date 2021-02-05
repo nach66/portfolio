b3d2da5abefac16bfa3c7a3256554a995e73e648
Nachala Vinograd <nach666@gmail.com>
2021-02-05 14:09:03 +0200

    ח

--- src/english/components/Prolog.js
+++ src/english/components/Prolog.js
@@ -3,11 +3,9 @@ import React from 'react'
 export default function Prolog() {
     return (
         <div className="prolog lang">
-            <p>If there is one thing that 2020 has taught us, it is that every business must have a website. The internet never shuts down: everything happens online, and you too need to be there!</p>
-            <p>With the most beautiful designs and the most eloquent sentences, you will be there to leave customers with an unforgettable impression, from the first entrance to your home site.</p>
-            <p>You need a beautiful and modern website, accessible and secure, fast even though it contains everything.</p>
-            <p>A site designed with the atmosphere that you are most interest in creating for the costumer who visits you, and gives him a quick and enjoyable access to use the service we are offering.</p>
-            <p>And I, by chance, develop websites just like that :)</p>
+            <p>If there is one thing that 2020 has taught us, it is that every business must have a website. The internet never shuts down: everything happens online, and you should be there as well!</p>
+            <p>With the most beautiful designs and the most eloquent sentences, you will be there to leave customers with an unforgettable impression, from their first entrance to your home site.</p>
+            <p>I can both develope and design for you a new modern website, fast and secure, with friendly user experience. You can see this website as an example ;)</p>
         </div>
     )
 }
