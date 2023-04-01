0637248927bcdacf494efa66348c74bcabd229de
Nachala Vinograd <nach666@gmail.com>
2023-04-02 01:26:51 +0300

    gg

--- src/hebrew/pages/Home.js
+++ src/hebrew/pages/Home.js
@@ -14,7 +14,7 @@ export default class Page extends React.Component {
     constructor(props){
         super(props)
         this.state = {open : true}
-        this.makeTimer()
+        // this.makeTimer()
     }
 
     makeTimer(){
@@ -34,7 +34,7 @@ export default class Page extends React.Component {
         <>
             <Modal 
                 open={open} 
-                // center={true}
+                center={true}
                 // focusTrapped={true}
                 // showCloseIcon={false}
                 onClose={this.onCloseModal}
